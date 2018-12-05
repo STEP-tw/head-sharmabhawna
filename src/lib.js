3bbbfd38c24ec6d9611900ef897d0917321fca08
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-12-05 18:30:18 +0530

    Enhanced functionality of varifyInputs

--- src/lib.js
+++ src/lib.js
@@ -6,13 +6,34 @@ const getInput = function() {
   return inputs;
 }
 
+const extractOption = function(choice) {
+  let option = "n";
+  if(choice.startsWith("-c")){
+    option = "c";
+  }
+  return option;
+}
+
 const varifyInputs = function(inputs) {
-  let result = { firstFile : inputs[0], linesToSlice : 10 };
+  let result = { option : "n", count : 10, files : inputs };
+  result.option = extractOption(inputs[0]);
+  if(inputs[0].startsWith("-")){
+    result.files = inputs.slice(1);
+    result.count = inputs[0][1];
+    if(inputs[0].length == 3){
+      result.count = inputs[0][2];
+    }
+    if(! inputs[1].match("[a-z]"+"[0-9]")){
+      result.count = inputs[1];
+      result.files = inputs.slice(2);
+    }
+  }
   return result;
 }
 
-const split= function(data, symbol) {
+const split = function(data, symbol) {
   return data.split(symbol);
 }
+
 
 module.exports = { split, getInput, varifyInputs };
--- test/libTest.js
+++ test/libTest.js
@@ -1,6 +1,6 @@
 const { equal, deepEqual } = require("assert");
 
-const { split } = require("../src/lib.js");
+const { split, varifyInputs } = require("../src/lib.js");
 
 describe("split", function(){
   it("should return empty array when data and splitor both are empty strings", function(){
@@ -17,4 +17,31 @@ describe("split", function(){
     deepEqual(split("ba", "a"), ["b",""]);
     deepEqual(split("ba", "b"), ["","a"]);
   });
+});
+
+describe("varifyInputs", function(){
+  it("should return object containing n as defult option and 10 as defult count value", function(){
+    deepEqual(varifyInputs(["file1"]), { option : "n", count : 10, files : ["file1"] } );
+    deepEqual(varifyInputs(["file1", "file2"]), { option : "n", count : 10, files : ["file1", "file2"] } );
+  });
+
+  it("should return object containing n as defult option and given count", function(){
+    deepEqual(varifyInputs(["-5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
+    deepEqual(varifyInputs(["-5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
+  });
+
+  it("should return object with given option and count", function(){
+    deepEqual(varifyInputs(["-n5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
+    deepEqual(varifyInputs(["-c5", "file1"]), { option : "c", count : "5", files : ["file1"] } );
+    deepEqual(varifyInputs(["-n5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
+    deepEqual(varifyInputs(["-c5", "file1", "file2"]), { option : "c", count : "5", files : ["file1", "file2"] } );
+  });
+
+  it("should return object with given option and count", function(){
+    deepEqual(varifyInputs(["-n", "5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
+    deepEqual(varifyInputs(["-c", "5", "file1"]), { option : "c", count : "5", files : ["file1"] } );
+    deepEqual(varifyInputs(["-n", "5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
+    deepEqual(varifyInputs(["-c", "5", "file1", "file2"]), { option : "c", count : "5", files : ["file1", "file2"] } );
+  });
+
 });
