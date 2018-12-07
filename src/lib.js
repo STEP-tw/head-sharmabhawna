aee5cb43773be897289ec3a5f8858a3e31d982e5
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-12-07 18:03:38 +0530

    Prittified code. Renamed heading to header and splitor to seperator.

--- src/lib.js
+++ src/lib.js
@@ -4,55 +4,62 @@ let byteCountError = "head: illegal byte count -- ";
 
 const extractOption = function(input) {
   let option = "n";
-  if(input.startsWith("-c")){
+  if (input.startsWith("-c")) {
     option = "c";
   }
   return option;
-}
+};
 
 const segregateInputs = function(inputs) {
-  let result = { option : "n", count : 10, files : inputs };
+  let result = { option: "n", count: 10, files: inputs };
   result.option = extractOption(inputs[0]);
 
-  if(inputs[0].startsWith("-")){
+  if (inputs[0].startsWith("-")) {
     result.files = inputs.slice(1);
     result.count = inputs[0].slice(1);
 
-    if(inputs[0].length >= 3 && inputs[0].match("[a-z]")){
+    if (inputs[0].length >= 3 && inputs[0].match("[a-z]")) {
       result.count = inputs[0].slice(2);
     }
 
-    if(! isNaN(inputs[1])){
+    if (!isNaN(inputs[1])) {
       result.count = inputs[1];
       result.files = inputs.slice(2);
     }
   }
   return result;
-}
+};
 
 const extractContent = function(fs, option, count, file) {
   let { readFileSync, existsSync } = fs;
-  let splitor = "\n";
-  if(option == "c"){
-    splitor = "";
+  let seperator = "\n";
+  if (option == "c") {
+    seperator = "";
   }
-  if( ! existsSync(file)){
-    return "head: "+file+": No such file or directory";
+  if (!existsSync(file)) {
+    return "head: " + file + ": No such file or directory";
   }
-  return readFileSync(file, "utf8").split(splitor).slice(0,count).join(splitor);
-}
+  return readFileSync(file, "utf8")
+    .split(seperator)
+    .slice(0, count)
+    .join(seperator);
+};
 
 const head = function(fs, { option, count, files }) {
-  if(count == 0 || isNaN(count)){
+  if (count == 0 || isNaN(count)) {
     return option == "n" ? lineCountError + count : byteCountError + count;
   }
   let extractData = extractContent.bind(null, fs, option, count);
-   if(files.length == 1){
-     return extractData(files[0]);
+  if (files.length == 1) {
+    return extractData(files[0]);
   }
-  return files.map(function(file){
-    let heading = "\n==> "+file+" <==\n";
-    return heading + extractData(file) } ).join("\n").slice(1);
-}
+  return files
+    .map(function(file) {
+      let header = "\n==> " + file + " <==\n";
+      return header + extractData(file);
+    })
+    .join("\n")
+    .slice(1);
+};
 
 module.exports = { extractOption, segregateInputs, head, extractContent };
