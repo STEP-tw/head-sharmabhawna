eacbc75156a3b7bb6dcf608f23354a063031fd26
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-11 01:08:58 +0530

    Added library for parsing inputs

---
+++ src/parsingInputLib.js
@@ -0,0 +1,29 @@
+const segregateInputs = function(inputs) {
+    let result = { option: "n", count: 10, files: inputs };
+    result.option = extractOption(inputs[0]);
+  
+    if (! inputs[0].startsWith("-")) {
+      return result;
+    }
+    result.files = inputs.slice(1);
+    result.count = inputs[0].slice(2);
+  
+    if (! isNaN(inputs[0][1])) {
+      result.count = inputs[0].slice(1);
+    }
+  
+    if (! isNaN(inputs[1])) {
+      result.count = inputs[1];
+      result.files = inputs.slice(2);
+    }
+    return result;
+};
+
+const extractOption = function(input) {
+    if (input.startsWith("-c")) {
+      return "c";
+    }
+    return "n";
+  };
+  
+module.exports = { segregateInputs, extractOption };
