a9c2927953cad1bf34ac272aa13627d41f8a72bc
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-11 10:53:58 +0530

    Introduced seperate test file for input parsing library

--- test/libTest.js
+++ test/libTest.js
@@ -1,54 +1,6 @@
 const { equal, deepEqual } = require("assert");
 
 const { extractContent, extractHeadContent, extractTailContent, head, tail } = require("../src/lib.js");
-  
-const { extractOption, segregateInputs } = require("../src/parsingInputLib");
-
-describe("segregateInputs", function(){
-  it("should return object containing n as defult option and 10 as defult count value", function(){
-    deepEqual(segregateInputs(["file1"]), { option : "n", count : 10, files : ["file1"] } );
-    deepEqual(segregateInputs(["file1", "file2"]), { option : "n", count : 10, files : ["file1", "file2"] } );
-  });
-
-  it("should return object containing n as defult option and given count", function(){
-    deepEqual(segregateInputs(["-5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
-    deepEqual(segregateInputs(["-12", "file1"]), { option : "n", count : "12", files : ["file1"] } );
-    deepEqual(segregateInputs(["-5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
-    deepEqual(segregateInputs(["-12", "file1", "file2"]), { option : "n", count : "12", files : ["file1", "file2"] } );
-  });
-
-  it("should return object with given option and count", function(){
-    deepEqual(segregateInputs(["-n5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
-    deepEqual(segregateInputs(["-n12", "file1"]), { option : "n", count : "12", files : ["file1"] } );
-    deepEqual(segregateInputs(["-c5", "file1"]), { option : "c", count : "5", files : ["file1"] } );
-    deepEqual(segregateInputs(["-c12", "file1"]), { option : "c", count : "12", files : ["file1"] } );
-    deepEqual(segregateInputs(["-n5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
-    deepEqual(segregateInputs(["-c5", "file1", "file2"]), { option : "c", count : "5", files : ["file1", "file2"] } );
-  });
-
-  it("should return object with given option and count", function(){
-    deepEqual(segregateInputs(["-n", "5", "file1"]), { option : "n", count : "5", files : ["file1"] } );
-    deepEqual(segregateInputs(["-n", "12", "file1"]), { option : "n", count : "12", files : ["file1"] } );
-    deepEqual(segregateInputs(["-c", "5", "file1"]), { option : "c", count : "5", files : ["file1"] } );
-    deepEqual(segregateInputs(["-c", "12", "file1"]), { option : "c", count : "12", files : ["file1"] } );
-    deepEqual(segregateInputs(["-n", "5", "file1", "file2"]), { option : "n", count : "5", files : ["file1", "file2"] } );
-    deepEqual(segregateInputs(["-c", "5", "file1", "file2"]), { option : "c", count : "5", files : ["file1", "file2"] } );
-  });
-});
-
-describe("extractOption", function(){
-  it("should return c if input starts with -c", function(){
-    equal(extractOption("-c"), "c");
-    equal(extractOption("-c5"), "c");
-  });
-
-  it("should return n if input does not start with -c", function(){
-    equal(extractOption("-5"), "n");
-    equal(extractOption("-n"), "n");
-    equal(extractOption("-n5"), "n");
-    equal(extractOption("file"), "n");
-  });
-});
 
 const areMatched = function(arg1, arg2) {
   return arg1 === arg2;
