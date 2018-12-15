eeb74b4ec015a0d392ed7fbab67d5945d4f605a1
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-15 10:17:10 +0530

    Prittified code

--- src/lib.js
+++ src/lib.js
@@ -1,12 +1,18 @@
 const selectDelimiter = function(option) {
- return option == "c" ? "" : "\n";
+  return option == "c" ? "" : "\n";
 };
 
-const extractRequiredContent = function(callingContext, fs, option, count, fileName) {
+const extractRequiredContent = function(
+  callingContext,
+  fs,
+  option,
+  count,
+  fileName
+) {
   let delimiter = selectDelimiter(option);
   let splittedContent = extractContent(fs, fileName).split(delimiter);
   let slicedContent = extractTailContent(splittedContent, count);
-  if(callingContext == "head"){
+  if (callingContext == "head") {
     slicedContent = extractHeadContent(splittedContent, count);
   }
   return slicedContent.join(delimiter);
@@ -18,8 +24,8 @@ const extractHeadContent = function(contents, count) {
 
 const extractTailContent = function(contents, count) {
   let lengthOfFile = contents.length;
-  let requiredCount = lengthOfFile-count;
-  if(count > lengthOfFile){
+  let requiredCount = lengthOfFile - count;
+  if (count > lengthOfFile) {
     requiredCount = 0;
   }
   return contents.slice(requiredCount);
@@ -34,34 +40,66 @@ const isInvalidCount = function(count) {
   return count == 0 || isNaN(count);
 };
 
-const extractSingleFileContent = function(callingContext, existanceCheckerFn, contentExtractorFn, file){
-  if(! existanceCheckerFn(file)){
-    return callingContext+": "+file+": No such file or directory";
+const extractSingleFileContent = function(
+  callingContext,
+  existanceCheckerFn,
+  contentExtractorFn,
+  file
+) {
+  if (!existanceCheckerFn(file)) {
+    return callingContext + ": " + file + ": No such file or directory";
   }
   return contentExtractorFn(file);
 };
 
-const generateHeader = function(fileName){
+const generateHeader = function(fileName) {
   return "\n==> " + fileName + " <==\n";
 };
 
-const extractMultipleFilesContent = function(callingContext, existanceCheckerFn, contentExtractorFn, files){
+const extractMultipleFilesContent = function(
+  callingContext,
+  existanceCheckerFn,
+  contentExtractorFn,
+  files
+) {
   return files
     .map(function(file) {
-      let data = extractSingleFileContent(callingContext, existanceCheckerFn, contentExtractorFn, file);
-      if(data.match(/: No such file or directory/)){
+      let data = extractSingleFileContent(
+        callingContext,
+        existanceCheckerFn,
+        contentExtractorFn,
+        file
+      );
+      if (data.match(/: No such file or directory/)) {
         return "\n" + data;
       }
       return generateHeader(file) + data;
-    }).join("\n").slice(1);
+    })
+    .join("\n")
+    .slice(1);
 };
 
-const applyRequiredFunc = function(callingContext, existanceCheckerFn, contentExtractorFn, files){
+const applyRequiredFunc = function(
+  callingContext,
+  existanceCheckerFn,
+  contentExtractorFn,
+  files
+) {
   if (files.length == 1) {
-    return extractSingleFileContent(callingContext, existanceCheckerFn, contentExtractorFn, files[0]);
+    return extractSingleFileContent(
+      callingContext,
+      existanceCheckerFn,
+      contentExtractorFn,
+      files[0]
+    );
   }
-  return extractMultipleFilesContent(callingContext, existanceCheckerFn, contentExtractorFn, files);
-}
+  return extractMultipleFilesContent(
+    callingContext,
+    existanceCheckerFn,
+    contentExtractorFn,
+    files
+  );
+};
 
 const head = function(fs, { option, count, files }) {
   let lineCountError = "head: illegal line count -- ";
@@ -70,21 +108,42 @@ const head = function(fs, { option, count, files }) {
 
   let { existsSync } = fs;
   if (isInvalidCount(count)) {
-    return option == "n" ? lineCountError + count : byteCountError + count; 
+    return option == "n" ? lineCountError + count : byteCountError + count;
   }
-   let contentExtractor = extractRequiredContent.bind(null, "head", fs, option, count);
-   return applyRequiredFunc("head", existsSync, contentExtractor, files);
+  let contentExtractor = extractRequiredContent.bind(
+    null,
+    "head",
+    fs,
+    option,
+    count
+  );
+  return applyRequiredFunc("head", existsSync, contentExtractor, files);
 };
 
 const tail = function(fs, { option, count, files }) {
   let offsetError = "tail: illegal offset -- ";
 
   let { existsSync } = fs;
-  if(isNaN(count)) {
+  if (isNaN(count)) {
     return offsetError + count;
   }
-  let contentExtractor = extractRequiredContent.bind(null, "tail", fs, option, count);
+  let contentExtractor = extractRequiredContent.bind(
+    null,
+    "tail",
+    fs,
+    option,
+    count
+  );
   return applyRequiredFunc("tail", existsSync, contentExtractor, files);
 };
 
-module.exports = { selectDelimiter, generateHeader, extractRequiredContent, extractContent, extractHeadContent, extractTailContent, head, tail };
+module.exports = {
+  selectDelimiter,
+  generateHeader,
+  extractRequiredContent,
+  extractContent,
+  extractHeadContent,
+  extractTailContent,
+  head,
+  tail
+};
--- test/libTest.js
+++ test/libTest.js
@@ -1,184 +1,276 @@
 const { equal, deepEqual } = require("assert");
 
-const { selectDelimiter, generateHeader, extractRequiredContent, extractContent, extractHeadContent, extractTailContent, head, tail } = require("../src/lib.js");
+const {
+  selectDelimiter,
+  generateHeader,
+  extractRequiredContent,
+  extractContent,
+  extractHeadContent,
+  extractTailContent,
+  head,
+  tail
+} = require("../src/lib.js");
 
-describe("selectDelimiter", function(){
-  it("should return new line character if n is given as option", function(){
+describe("selectDelimiter", function() {
+  it("should return new line character if n is given as option", function() {
     equal(selectDelimiter("n"), "\n");
   });
 
-  it("should return empty string if n is given as option", function(){
+  it("should return empty string if n is given as option", function() {
     equal(selectDelimiter("c"), "");
   });
 });
 
-describe("generateHeader", function(){
-  it("should return header of given file name", function(){
-    equal(generateHeader("symbols"), "\n==> symbols <==\n")
-    equal(generateHeader("letters"), "\n==> letters <==\n")
+describe("generateHeader", function() {
+  it("should return header of given file name", function() {
+    equal(generateHeader("symbols"), "\n==> symbols <==\n");
+    equal(generateHeader("letters"), "\n==> letters <==\n");
   });
 });
 
 const areMatched = function(arg1, arg2) {
   return arg1 === arg2;
-}
+};
 
 const mockedReader = function(expectedFileName, encoding, content) {
-  return function(actualFileName, encoding){
-    if(areMatched(expectedFileName, actualFileName)){
+  return function(actualFileName, encoding) {
+    if (areMatched(expectedFileName, actualFileName)) {
       return content;
     }
-  }
-}
+  };
+};
 
 const mockedExistanceChecker = function(expectedFileName) {
-  return function(actualFileName){
+  return function(actualFileName) {
     return areMatched(expectedFileName, actualFileName);
-  }
-}
+  };
+};
 
 const symbolsReader = mockedReader("symbols", "utf8", "*\n@\n%\n$\n#");
 
 const symblosExistanceChecker = mockedExistanceChecker("symbols");
 
-const mockedFS = { readFileSync : symbolsReader, existsSync : symblosExistanceChecker };
+const mockedFS = {
+  readFileSync: symbolsReader,
+  existsSync: symblosExistanceChecker
+};
 
-describe("extractContent", function(){
-  it("should return full content of file", function(){
+describe("extractContent", function() {
+  it("should return full content of file", function() {
     equal(extractContent(mockedFS, "symbols"), "*\n@\n%\n$\n#");
   });
 });
 
-describe("extractHeadContent", function(){
-  it("should return given count of elements from start of an array", function(){
+describe("extractHeadContent", function() {
+  it("should return given count of elements from start of an array", function() {
     deepEqual(extractHeadContent(["*", "@", "%", "$", "#"], 1), ["*"]);
     deepEqual(extractHeadContent(["*", "@", "%", "$", "#"], 2), ["*", "@"]);
   });
-
 });
 
-describe("extractTailContent", function(){
-  it("should return given count of elements from end of an array", function(){
+describe("extractTailContent", function() {
+  it("should return given count of elements from end of an array", function() {
     deepEqual(extractTailContent(["*", "@", "%", "$", "#"], 1), ["#"]);
     deepEqual(extractTailContent(["*", "@", "%", "$", "#"], 2), ["$", "#"]);
   });
-
 });
 
-describe("extractRequiredContent", function(){
-  it("should return count of lines from top if calling context is head and and option is n", function(){
-    equal(extractRequiredContent("head", mockedFS, "n", 1, "symbols"), "*")
-    equal(extractRequiredContent("head", mockedFS, "n", 2, "symbols"), "*\n@")
+describe("extractRequiredContent", function() {
+  it("should return count of lines from top if calling context is head and and option is n", function() {
+    equal(extractRequiredContent("head", mockedFS, "n", 1, "symbols"), "*");
+    equal(extractRequiredContent("head", mockedFS, "n", 2, "symbols"), "*\n@");
   });
 
-  it("should return count of bytes from top if calling context is head and and option is c", function(){
-    equal(extractRequiredContent("head", mockedFS, "c", 1, "symbols"), "*")
-    equal(extractRequiredContent("head", mockedFS, "c", 2, "symbols"), "*\n")
+  it("should return count of bytes from top if calling context is head and and option is c", function() {
+    equal(extractRequiredContent("head", mockedFS, "c", 1, "symbols"), "*");
+    equal(extractRequiredContent("head", mockedFS, "c", 2, "symbols"), "*\n");
   });
 
-  it("should return count of lines from bottom if calling context is tail and and option is n", function(){
-    equal(extractRequiredContent("tail", mockedFS, "n", 1, "symbols"), "#")
-    equal(extractRequiredContent("tail", mockedFS, "n", 2, "symbols"), "$\n#")
+  it("should return count of lines from bottom if calling context is tail and and option is n", function() {
+    equal(extractRequiredContent("tail", mockedFS, "n", 1, "symbols"), "#");
+    equal(extractRequiredContent("tail", mockedFS, "n", 2, "symbols"), "$\n#");
   });
 
-  it("should return count of bytes from bottom if calling context is tail and and option is c", function(){
-    equal(extractRequiredContent("tail", mockedFS, "c", 1, "symbols"), "#")
-    equal(extractRequiredContent("tail", mockedFS, "c", 2, "symbols"), "\n#")
-  });
-
-});
-
-describe("head", function(){
-  it("should throw error that count is zero", function(){
-    equal(head(mockedFS, { option : "c", count : 0, files : ["symbols"] }), "head: illegal byte count -- 0");
-    equal(head(mockedFS, { option : "n", count : 0, files : ["symbols"] }), "head: illegal line count -- 0");
-  });
-
-  it("should throw error that count is invalid or non-numeric", function(){
-    equal(head(mockedFS, { option : "c", count : "1x", files : ["symbols"] }), "head: illegal byte count -- 1x");
-    equal(head(mockedFS, { option : "n", count : "1x", files : ["symbols"] }), "head: illegal line count -- 1x");
-  });
-
-  it("should return given count of bytes of file from top if option is c", function(){
-    equal(head(mockedFS, { option : "c", count : 1, files : ["symbols"] }), "*");
-    equal(head(mockedFS, { option : "c", count : 2, files : ["symbols"] }), "*\n");
-  });
-
-  it("should return given count of lines of file from top if option is n", function(){
-    equal(head(mockedFS, { option : "n", count : 1, files : ["symbols"] }), "*");
-    equal(head(mockedFS, { option : "n", count : 2, files : ["symbols"] }), "*\n@");
-  });
-
-  it("should return given count of bytes of all files from top seperated by file names if option is c", function(){
-    equal(head(mockedFS, { option : "c", count : 1, files : ["symbols", "symbols"] }), "==> symbols <==\n*\n\n==> symbols <==\n*");
-    equal(head(mockedFS, { option : "c", count : 2, files : ["symbols", "symbols"] }), "==> symbols <==\n*\n\n\n==> symbols <==\n*\n");
-  });
-  
-  it("should return given count of lines of all files from top seperated by file names if option is n", function(){
-    equal(head(mockedFS, { option : "n", count : 1, files : ["symbols", "symbols"] }), "==> symbols <==\n*\n\n==> symbols <==\n*");
-    equal(head(mockedFS, { option : "n", count : 2, files : ["symbols", "symbols"] }), "==> symbols <==\n*\n@\n\n==> symbols <==\n*\n@");
-  });
-
-  it("should throw error if file is not present", function(){
-    equal(head(mockedFS, { option : "c", count : 1, files : ["letters"] }), "head: letters: No such file or directory");
-    equal(head(mockedFS, { option : "n", count : 2, files : ["letters"] }), "head: letters: No such file or directory");
-  });
-
-  it("should throw error for files those are not present and return given count of bytes of existing files from top", function(){
-    equal(head(mockedFS, { option : "c", count : 1, files : ["letters", "symbols"] }), "head: letters: No such file or directory\n\n==> symbols <==\n*");
-    equal(head(mockedFS, { option : "c", count : 1, files : ["symbols", "letters"] }), "==> symbols <==\n*\n\nhead: letters: No such file or directory");
-  });
-
-  it("should throw error for files those are not present and return given count of lines of existing files from top", function(){
-    equal(head(mockedFS, { option : "n", count : 1, files : ["letters", "symbols"] }), "head: letters: No such file or directory\n\n==> symbols <==\n*");
-    equal(head(mockedFS, { option : "n", count : 1, files : ["symbols", "letters"] }), "==> symbols <==\n*\n\nhead: letters: No such file or directory");
+  it("should return count of bytes from bottom if calling context is tail and and option is c", function() {
+    equal(extractRequiredContent("tail", mockedFS, "c", 1, "symbols"), "#");
+    equal(extractRequiredContent("tail", mockedFS, "c", 2, "symbols"), "\n#");
   });
 });
 
-describe("tail", function(){
-  it("should return empty string if count is zero", function(){
-    equal(tail(mockedFS, { option : "c", count : "0", files : ["symbols"] }), "");
-    equal(tail(mockedFS, { option : "n", count : "0", files : ["symbols"] }), "");
+describe("head", function() {
+  it("should throw error that count is zero", function() {
+    equal(
+      head(mockedFS, { option: "c", count: 0, files: ["symbols"] }),
+      "head: illegal byte count -- 0"
+    );
+    equal(
+      head(mockedFS, { option: "n", count: 0, files: ["symbols"] }),
+      "head: illegal line count -- 0"
+    );
   });
 
-  it("should throw error that count is invalid or non-numeric", function(){
-    equal(tail(mockedFS, { option : "c", count : "1x", files : ["symbols"] }), "tail: illegal offset -- 1x");
-    equal(tail(mockedFS, { option : "n", count : "1x", files : ["symbols"] }), "tail: illegal offset -- 1x");
+  it("should throw error that count is invalid or non-numeric", function() {
+    equal(
+      head(mockedFS, { option: "c", count: "1x", files: ["symbols"] }),
+      "head: illegal byte count -- 1x"
+    );
+    equal(
+      head(mockedFS, { option: "n", count: "1x", files: ["symbols"] }),
+      "head: illegal line count -- 1x"
+    );
   });
 
-  it("should return given count of bytes of file from bottom if option is c", function(){
-    equal(tail(mockedFS, { option : "c", count : 1, files : ["symbols"] }), "#");
-    equal(tail(mockedFS, { option : "c", count : 2, files : ["symbols"] }), "\n#");
+  it("should return given count of bytes of file from top if option is c", function() {
+    equal(head(mockedFS, { option: "c", count: 1, files: ["symbols"] }), "*");
+    equal(head(mockedFS, { option: "c", count: 2, files: ["symbols"] }), "*\n");
   });
 
-  it("should return given count of lines of file from bottom if option is n", function(){
-    equal(tail(mockedFS, { option : "n", count : 1, files : ["symbols"] }), "#");
-    equal(tail(mockedFS, { option : "n", count : 2, files : ["symbols"] }), "$\n#");
+  it("should return given count of lines of file from top if option is n", function() {
+    equal(head(mockedFS, { option: "n", count: 1, files: ["symbols"] }), "*");
+    equal(
+      head(mockedFS, { option: "n", count: 2, files: ["symbols"] }),
+      "*\n@"
+    );
   });
 
-  it("should return given count of bytes of all files from bottom seperated by file names if option is c", function(){
-    equal(tail(mockedFS, { option : "c", count : 1, files : ["symbols", "symbols"] }), "==> symbols <==\n#\n\n==> symbols <==\n#");
-    equal(tail(mockedFS, { option : "c", count : 2, files : ["symbols", "symbols"] }), "==> symbols <==\n\n#\n\n==> symbols <==\n\n#");
-  });
-  
-  it("should return given count of lines of all files from bottom seperated by files names if option is n", function(){
-    equal(tail(mockedFS, { option : "n", count : 1, files : ["symbols", "symbols"] }), "==> symbols <==\n#\n\n==> symbols <==\n#");
-    equal(tail(mockedFS, { option : "n", count : 2, files : ["symbols", "symbols"] }), "==> symbols <==\n$\n#\n\n==> symbols <==\n$\n#");
-  });
-
-  it("should throw error if file is not present", function(){
-    equal(tail(mockedFS, { option : "c", count : 1, files : ["letters"] }), "tail: letters: No such file or directory");
-    equal(tail(mockedFS, { option : "n", count : 2, files : ["letters"] }), "tail: letters: No such file or directory");
+  it("should return given count of bytes of all files from top seperated by file names if option is c", function() {
+    equal(
+      head(mockedFS, { option: "c", count: 1, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n*\n\n==> symbols <==\n*"
+    );
+    equal(
+      head(mockedFS, { option: "c", count: 2, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n*\n\n\n==> symbols <==\n*\n"
+    );
   });
 
-  it("should throw error for files those are not present and return given count of bytes of existing files from bottom", function(){
-    equal(tail(mockedFS, { option : "c", count : 1, files : ["letters", "symbols"] }), "tail: letters: No such file or directory\n\n==> symbols <==\n#");
-    equal(tail(mockedFS, { option : "c", count : 1, files : ["symbols", "letters"] }), "==> symbols <==\n#\n\ntail: letters: No such file or directory");
+  it("should return given count of lines of all files from top seperated by file names if option is n", function() {
+    equal(
+      head(mockedFS, { option: "n", count: 1, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n*\n\n==> symbols <==\n*"
+    );
+    equal(
+      head(mockedFS, { option: "n", count: 2, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n*\n@\n\n==> symbols <==\n*\n@"
+    );
   });
 
-  it("should throw error for files those are not present and return given count of lines of existing files from bottom", function(){
-    equal(tail(mockedFS, { option : "n", count : 1, files : ["letters", "symbols"] }), "tail: letters: No such file or directory\n\n==> symbols <==\n#");
-    equal(tail(mockedFS, { option : "n", count : 1, files : ["symbols", "letters"] }), "==> symbols <==\n#\n\ntail: letters: No such file or directory");
+  it("should throw error if file is not present", function() {
+    equal(
+      head(mockedFS, { option: "c", count: 1, files: ["letters"] }),
+      "head: letters: No such file or directory"
+    );
+    equal(
+      head(mockedFS, { option: "n", count: 2, files: ["letters"] }),
+      "head: letters: No such file or directory"
+    );
   });
 
+  it("should throw error for files those are not present and return given count of bytes of existing files from top", function() {
+    equal(
+      head(mockedFS, { option: "c", count: 1, files: ["letters", "symbols"] }),
+      "head: letters: No such file or directory\n\n==> symbols <==\n*"
+    );
+    equal(
+      head(mockedFS, { option: "c", count: 1, files: ["symbols", "letters"] }),
+      "==> symbols <==\n*\n\nhead: letters: No such file or directory"
+    );
+  });
+
+  it("should throw error for files those are not present and return given count of lines of existing files from top", function() {
+    equal(
+      head(mockedFS, { option: "n", count: 1, files: ["letters", "symbols"] }),
+      "head: letters: No such file or directory\n\n==> symbols <==\n*"
+    );
+    equal(
+      head(mockedFS, { option: "n", count: 1, files: ["symbols", "letters"] }),
+      "==> symbols <==\n*\n\nhead: letters: No such file or directory"
+    );
+  });
+});
+
+describe("tail", function() {
+  it("should return empty string if count is zero", function() {
+    equal(tail(mockedFS, { option: "c", count: "0", files: ["symbols"] }), "");
+    equal(tail(mockedFS, { option: "n", count: "0", files: ["symbols"] }), "");
+  });
+
+  it("should throw error that count is invalid or non-numeric", function() {
+    equal(
+      tail(mockedFS, { option: "c", count: "1x", files: ["symbols"] }),
+      "tail: illegal offset -- 1x"
+    );
+    equal(
+      tail(mockedFS, { option: "n", count: "1x", files: ["symbols"] }),
+      "tail: illegal offset -- 1x"
+    );
+  });
+
+  it("should return given count of bytes of file from bottom if option is c", function() {
+    equal(tail(mockedFS, { option: "c", count: 1, files: ["symbols"] }), "#");
+    equal(tail(mockedFS, { option: "c", count: 2, files: ["symbols"] }), "\n#");
+  });
+
+  it("should return given count of lines of file from bottom if option is n", function() {
+    equal(tail(mockedFS, { option: "n", count: 1, files: ["symbols"] }), "#");
+    equal(
+      tail(mockedFS, { option: "n", count: 2, files: ["symbols"] }),
+      "$\n#"
+    );
+  });
+
+  it("should return given count of bytes of all files from bottom seperated by file names if option is c", function() {
+    equal(
+      tail(mockedFS, { option: "c", count: 1, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n#\n\n==> symbols <==\n#"
+    );
+    equal(
+      tail(mockedFS, { option: "c", count: 2, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n\n#\n\n==> symbols <==\n\n#"
+    );
+  });
+
+  it("should return given count of lines of all files from bottom seperated by files names if option is n", function() {
+    equal(
+      tail(mockedFS, { option: "n", count: 1, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n#\n\n==> symbols <==\n#"
+    );
+    equal(
+      tail(mockedFS, { option: "n", count: 2, files: ["symbols", "symbols"] }),
+      "==> symbols <==\n$\n#\n\n==> symbols <==\n$\n#"
+    );
+  });
+
+  it("should throw error if file is not present", function() {
+    equal(
+      tail(mockedFS, { option: "c", count: 1, files: ["letters"] }),
+      "tail: letters: No such file or directory"
+    );
+    equal(
+      tail(mockedFS, { option: "n", count: 2, files: ["letters"] }),
+      "tail: letters: No such file or directory"
+    );
+  });
+
+  it("should throw error for files those are not present and return given count of bytes of existing files from bottom", function() {
+    equal(
+      tail(mockedFS, { option: "c", count: 1, files: ["letters", "symbols"] }),
+      "tail: letters: No such file or directory\n\n==> symbols <==\n#"
+    );
+    equal(
+      tail(mockedFS, { option: "c", count: 1, files: ["symbols", "letters"] }),
+      "==> symbols <==\n#\n\ntail: letters: No such file or directory"
+    );
+  });
+
+  it("should throw error for files those are not present and return given count of lines of existing files from bottom", function() {
+    equal(
+      tail(mockedFS, { option: "n", count: 1, files: ["letters", "symbols"] }),
+      "tail: letters: No such file or directory\n\n==> symbols <==\n#"
+    );
+    equal(
+      tail(mockedFS, { option: "n", count: 1, files: ["symbols", "letters"] }),
+      "==> symbols <==\n#\n\ntail: letters: No such file or directory"
+    );
+  });
 });
