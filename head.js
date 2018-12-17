aebc3a22deedcaed519dd086a4ddb91e1e15382b
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-17 15:30:44 +0530

    Removed unnecessary commented lines and blank lines

--- head.js
+++ head.js
@@ -1,23 +1,5 @@
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-*/
-
 const fs = require("fs");
-
 const { head } = require("./src/lib.js");
-
 const { segregateInputs } = require("./src/parsingInputLib.js");
 
 const main = function() {
