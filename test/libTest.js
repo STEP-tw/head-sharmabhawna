d239df517cadf72bd28e0e687f03c26fff928ac1
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-18 22:03:57 +0530

    Introduced tests for empty files,
    removed redudant tests and modified tests by grouping common tests
    of head

--- test/libTest.js
+++ test/libTest.js
@@ -266,113 +266,86 @@ describe("extractFilesContent", function () {
 });
 
 describe("head", function () {
-  it("should throw byte count error when option is c and count is zero", function () {
-    let parsedInputs = { option: "c", count: 0, files: ["symbols"] };
-    let expectedOutput = "head: illegal byte count -- 0";
+  describe("should throw line count error when option is n", function () {
+    it("and count is zero", function () {
+      let parsedInputs = { option: "n", count: 0, files: ["symbols"] };
+      let expectedOutput = "head: illegal line count -- 0";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(head(mockedFS, parsedInputs), expectedOutput);
+    it("and count is not a number", function () {
+      let parsedInputs = { option: "n", count: "1x", files: ["symbols"] };
+      let expectedOutput = "head: illegal line count -- 1x";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
   });
 
-  it("should throw line count error when option is n count is zero", function () {
-    let parsedInputs = { option: "n", count: 0, files: ["symbols"] };
-    let expectedOutput = "head: illegal line count -- 0";
+  describe("should throw byte count error when option is c", function () {
+    it("and count is zero", function () {
+      let parsedInputs = { option: "c", count: 0, files: ["symbols"] };
+      let expectedOutput = "head: illegal byte count -- 0";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(head(mockedFS, parsedInputs), expectedOutput);
+    it("and count is not a number", function () {
+      let parsedInputs = { option: "c", count: "1x", files: ["symbols"] };
+      let expectedOutput = "head: illegal byte count -- 1x";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
   });
 
-  it("should throw byte count error when option is c and count is not a number", function () {
-    let parsedInputs = { option: "c", count: "1x", files: ["symbols"] };
-    let expectedOutput = "head: illegal byte count -- 1x";
+  describe("when single file is given", function () {
+    it("should throw existance error for non-existing file", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["letters"] };
+      let expectedOutput = "head: letters: No such file or directory";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(head(mockedFS, parsedInputs), expectedOutput);
+    it("should return empty string when file is empty", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["numbers"] };
+      equal(head(mockedFS, parsedInputs), "");
+    });
+
+    it("should return first n lines of file when option is n", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["symbols"] };
+      equal(head(mockedFS, parsedInputs), "*\n@");
+    });
+
+    it("should return first n bytes of file when option is c", function () {
+      let parsedInputs = { option: "c", count: 2, files: ["symbols"] };
+      equal(head(mockedFS, parsedInputs), "*\n");
+    });
   });
 
-  it("should throw line count error when option is n and count is not a number", function () {
-    let parsedInputs = { option: "n", count: "1x", files: ["symbols"] };
-    let expectedOutput = "head: illegal line count -- 1x";
+  describe("when multiple files are given", function () {
+    it("should throw existance error for non-existing files", function () {
+      let parsedInputs = { option: "c", count: 1, files: ["letters", "characters"] };
+      let expectedOutput = "head: letters: No such file or directory\nhead: characters: No such file or directory"
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
+    it("shpuld return blank lines seperated by file names when files are empty", function () {
+      let parsedInputs = { option: "n", count: 1, files: ["numbers", "operators"] };
+      let expectedOutput = "==> numbers <==\n\n==> operators <==\n";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-  it("should return first byte of file", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols"] };
-    equal(head(mockedFS, parsedInputs), "*");
-  });
+    it("should throw existance error for non-existing files and return first n lines of existing files", function () {
+      let parsedInputs = { option: "n", count: 1, files: ["letters", "symbols", "characters"] };
+      let expectedOutput = "head: letters: No such file or directory\n==> symbols <==\n*\nhead: characters: No such file or directory";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
 
-  it("should return first two bytes of file", function () {
-    let parsedInputs = { option: "c", count: 2, files: ["symbols"] };
-    equal(head(mockedFS, parsedInputs), "*\n");
-  });
-
-  it("should return first line of file", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols"] };
-    equal(head(mockedFS, parsedInputs), "*");
-  });
-
-  it("should return first two lines of file", function () {
-    let parsedInputs = { option: "n", count: 2, files: ["symbols"] };
-    equal(head(mockedFS, parsedInputs), "*\n@");
-  });
-
-  it("should return first byte of all files seperated by file names", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols", "vowels"] };
-    let expectedOutput = "==> symbols <==\n*\n==> vowels <==\na";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return first two bytes of all files seperated by file names", function () {
-    let parsedInputs = { option: "c", count: 2, files: ["vowels", "symbols"] };
-    let expectedOutput = "==> vowels <==\na\n\n==> symbols <==\n*\n";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return first line of all files seperated by file names", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols", "vowels"] };
-    let expectedOutput = "==> symbols <==\n*\n==> vowels <==\na";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return first two lines of all files seperated by file names", function () {
-    let parsedInputs = { option: "n", count: 2, files: ["vowels", "symbols"] };
-    let expectedOutput = "==> vowels <==\na\ne\n==> symbols <==\n*\n@";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should throw error that file is not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters"] };
-    let expectedOutput = "head: letters: No such file or directory";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should throw error that files are not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters", "characters"] };
-    let expectedOutput = "head: letters: No such file or directory\nhead: characters: No such file or directory"
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should throw error for file that is not present and then return first byte of existing file", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters", "symbols"] };
-    let expectedOutput = "head: letters: No such file or directory\n==> symbols <==\n*";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should return first byte of existing file and then throw error for file that is not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols", "letters"] };
-    let expectedOutput = "==> symbols <==\n*\nhead: letters: No such file or directory";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should throw error for file that is not present and then return first line of existing file", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["letters", "symbols"] };
-    let expectedOutput = "head: letters: No such file or directory\n==> symbols <==\n*";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should return first line of existing file and then throw error for file that is not present", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols", "letters"] };
-    let expectedOutput = "==> symbols <==\n*\nhead: letters: No such file or directory";
-    equal(head(mockedFS, parsedInputs), expectedOutput);
-
+    it("should return first n lines of all files seperated by file names when option is n", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["vowels", "symbols"] };
+      let expectedOutput = "==> vowels <==\na\ne\n==> symbols <==\n*\n@";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
+    it("should return first n bytes of all files seperated by file names when option is c", function () {
+      let parsedInputs = { option: "c", count: 2, files: ["vowels", "symbols"] };
+      let expectedOutput = "==> vowels <==\na\n\n==> symbols <==\n*\n";
+      equal(head(mockedFS, parsedInputs), expectedOutput);
+    });
   });
 });
 
