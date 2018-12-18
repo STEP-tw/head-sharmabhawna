6535ad9bb75622801bb8e85ee22b8b997df4a247
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-18 22:25:00 +0530

    Introduced tests for empty files,
    removed redudant tests and modified tests by grouping common tests
    of tail

--- test/libTest.js
+++ test/libTest.js
@@ -350,112 +350,83 @@ describe("head", function () {
 });
 
 describe("tail", function () {
-  it("should return empty string when option is c count is zero", function () {
-    let parsedInputs = { option: "c", count: 0, files: ["symbols"] };
-    let expectedOutput = "";
+  describe("should throw offset error when count is not a number", function () {
+    it("and option is n", function () {
+      let parsedInputs = { option: "n", count: "1x", files: ["symbols"] };
+      let expectedOutput = "tail: illegal offset -- 1x";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
+    it("and option is c", function () {
+      let parsedInputs = { option: "c", count: "1x", files: ["symbols"] };
+      let expectedOutput = "tail: illegal offset -- 1x";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
   });
 
-  it("should return empty string when option is n and count is zero", function () {
-    let parsedInputs = { option: "n", count: 0, files: ["symbols"] };
-    let expectedOutput = "";
+  describe("should return empty string when count is 0", function () {
+    it("and option is n", function () {
+      let parsedInputs = { option: "n", count: 0, files: ["symbols"] };
+      equal(tail(mockedFS, parsedInputs), "");
+    });
 
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
+    it("and option is n", function () {
+      let parsedInputs = { option: "c", count: 0, files: ["symbols"] };
+      equal(tail(mockedFS, parsedInputs), "");
+    });
   });
 
-  it("should throw offset error if option is n and count is not a number", function () {
-    let parsedInputs = { option: "c", count: "1x", files: ["symbols"] };
-    let expectedOutput = "tail: illegal offset -- 1x";
+  describe("when single file is given", function () {
+    it("should throw existance error for non-existing file", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["letters"] };
+      let expectedOutput = "tail: letters: No such file or directory";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
+    it("should return empty string when file is empty", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["numbers"] };
+      equal(tail(mockedFS, parsedInputs), "");
+    });
+
+    it("should return last n lines of file when option is n", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["symbols"] };
+      equal(tail(mockedFS, parsedInputs), "$\n#");
+    });
+
+    it("should return last n bytes of file when option is c", function () {
+      let parsedInputs = { option: "c", count: 2, files: ["symbols"] };
+      equal(tail(mockedFS, parsedInputs), "\n#");
+    });
   });
 
-  it("should throw offset error if option is n and count is not a number", function () {
-    let parsedInputs = { option: "n", count: "1x", files: ["symbols"] };
-    let expectedOutput = "tail: illegal offset -- 1x";
+  describe("when multiple files are given", function () {
+    it("should throw existance error for non-existing files", function () {
+      let parsedInputs = { option: "c", count: 1, files: ["letters", "characters"] };
+      let expectedOutput = "tail: letters: No such file or directory\ntail: characters: No such file or directory"
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
 
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
+    it("shpuld return blank lines seperated by file names when files are empty", function () {
+      let parsedInputs = { option: "n", count: 1, files: ["numbers", "operators"] };
+      let expectedOutput = "==> numbers <==\n\n==> operators <==\n";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
+
+    it("should throw existance error for non-existing files and return last n lines of existing files", function () {
+      let parsedInputs = { option: "n", count: 1, files: ["letters", "symbols", "characters"] };
+      let expectedOutput = "tail: letters: No such file or directory\n==> symbols <==\n#\ntail: characters: No such file or directory";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
+
+    it("should return last n lines of all files seperated by file names when option is n", function () {
+      let parsedInputs = { option: "n", count: 2, files: ["vowels", "symbols"] };
+      let expectedOutput = "==> vowels <==\no\nu\n==> symbols <==\n$\n#";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
+    it("should return last n bytes of all files seperated by file names when option is c", function () {
+      let parsedInputs = { option: "c", count: 2, files: ["vowels", "symbols"] };
+      let expectedOutput = "==> vowels <==\n\nu\n==> symbols <==\n\n#";
+      equal(tail(mockedFS, parsedInputs), expectedOutput);
+    });
   });
-
-  it("should return last byte of file", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols"] };
-    equal(tail(mockedFS, parsedInputs), "#");
-  });
-
-  it("should return last two bytes of file", function () {
-    let parsedInputs = { option: "c", count: 2, files: ["symbols"] };
-    equal(tail(mockedFS, parsedInputs), "\n#");
-  });
-
-  it("should return last line of file", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols"] };
-    equal(tail(mockedFS, parsedInputs), "#");
-  });
-
-  it("should return last two lines of file", function () {
-    let parsedInputs = { option: "n", count: 2, files: ["symbols"] };
-    equal(tail(mockedFS, parsedInputs), "$\n#");
-  });
-
-  it("should return last byte of all files seperated by file names", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols", "vowels"] };
-    let expectedOutput = "==> symbols <==\n#\n==> vowels <==\nu";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return last two bytes of all files seperated by file names", function () {
-    let parsedInputs = { option: "c", count: 2, files: ["vowels", "symbols"] };
-    let expectedOutput = "==> vowels <==\n\nu\n==> symbols <==\n\n#";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return last line of all files seperated by file names", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols", "vowels"] };
-    let expectedOutput = "==> symbols <==\n#\n==> vowels <==\nu";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should return last two lines of all files seperated by file names", function () {
-    let parsedInputs = { option: "n", count: 2, files: ["vowels", "symbols"] };
-    let expectedOutput = "==> vowels <==\no\nu\n==> symbols <==\n$\n#";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should throw error that file is not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters"] };
-    let expectedOutput = "tail: letters: No such file or directory";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("should throw error that files are not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters", "characters"] };
-    let expectedOutput = "tail: letters: No such file or directory\ntail: characters: No such file or directory"
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should throw error for file that is not present and then return last byte of existing file", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["letters", "symbols"] };
-    let expectedOutput = "tail: letters: No such file or directory\n==> symbols <==\n#";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should return last byte of existing file and then throw error for file that is not present", function () {
-    let parsedInputs = { option: "c", count: 1, files: ["symbols", "letters"] };
-    let expectedOutput = "==> symbols <==\n#\ntail: letters: No such file or directory";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should throw error for file that is not present and then return last line of existing file", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["letters", "symbols"] };
-    let expectedOutput = "tail: letters: No such file or directory\n==> symbols <==\n#";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
-  it("firstly should return last line of existing file and then throw error for file that is not present", function () {
-    let parsedInputs = { option: "n", count: 1, files: ["symbols", "letters"] };
-    let expectedOutput = "==> symbols <==\n#\ntail: letters: No such file or directory";
-    equal(tail(mockedFS, parsedInputs), expectedOutput);
-  });
-
 });
