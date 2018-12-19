5793e838b0f2ecd023cf1b23a92a70421aa905b9
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-19 15:17:38 +0530

    Introduced tests for addHeader

--- test/formatTextTest.js
+++ test/formatTextTest.js
@@ -1,8 +1,22 @@
 const { equal } = require("assert");
-const { generateHeader } = require("../src/formatText.js");
+const { generateHeader, addHeader } = require("../src/formatText.js");
 
 describe("generateHeader", function () {
     it("should return header of given file name", function () {
         equal(generateHeader("symbols"), "==> symbols <==");
+    });
+});
+
+describe("addHeader", function () {
+    it("should return text without header when text is existance error", function () {
+        let text = "head: letters: No such file or directory";
+        let expectedOutput = "head: letters: No such file or directory";
+        equal(addHeader("letters", text), expectedOutput);
+    });
+
+    it("should return text with file name as header", function () {
+        let text = "$\n%";
+        let expectedOutput = "==> symbols <==\n$\n%";
+        equal(addHeader("symbols", text), expectedOutput);
     });
 });
