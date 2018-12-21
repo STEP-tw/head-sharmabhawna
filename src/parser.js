854382539abc78f15b5fe7ede4ba5a791fc910d6
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-21 12:04:21 +0530

    Modified input parser

--- src/parser.js
+++ src/parser.js
@@ -9,37 +9,36 @@ const hasOptionWithcount = function (firstArg) {
   return firstArg.length > 2;
 };
 
-const getDefaultOptionAndCount = function (userInputs) {
-  return { option: "n", count: 10, files: userInputs };
+const createFormat = function (option, count, files) {
+  return { option, count, files };
 };
-
-const getDefaultOption = function (userInputs) {
-  return { option: "n", count: userInputs[0].slice(1), files: userInputs.slice(1) };
-};
-
-const pasreOptionWithCount = function (userInputs) {
-  return { option: userInputs[0].slice(1, 2), count: userInputs[0].slice(2), files: userInputs.slice(1) };
-};
-
-const pasreOptionWithoutCount = function (userInputs) {
-  return { option: userInputs[0].slice(1), count: userInputs[1], files: userInputs.slice(2) };
-};
-
 const parse = function (userInputs) {
   let firstArg = userInputs[0];
+  let secondArg = userInputs[1];
+  let option = "n";
+  let count = 10;
+  let files = userInputs;
+
   if (onlyFilesSpecified(firstArg)) {
-    return getDefaultOptionAndCount(userInputs);
+    return createFormat(option, count, files);
   }
+  files = userInputs.slice(1);
 
   if (hasNoOption(firstArg)) {
-    return getDefaultOption(userInputs);
+    let count = firstArg.slice(1);
+    return createFormat(option, count, files);
   }
 
   if (hasOptionWithcount(firstArg)) {
-    return pasreOptionWithCount(userInputs);
+    let option = firstArg.slice(1, 2);
+    let count = firstArg.slice(2);
+    return createFormat(option, count, files);
   }
+  option = firstArg.slice(1);
+  count = secondArg;
+  files = userInputs.slice(2);
 
-  return pasreOptionWithoutCount(userInputs);
+  return createFormat(option, count, files);
 };
 
 module.exports = { parse };
