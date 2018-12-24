3faec617e08d09b8f562034215ef4ac9f6c66518
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-25 02:00:37 +0530

    Modified countOffsetError

--- src/errors.js
+++ src/errors.js
@@ -7,20 +7,25 @@ const isIllegal = function (option) {
 };
 
 const countOffsetError = function (callingContext, option, count) {
-    let offsetError = "tail: illegal offset -- ";
-    let lineCountError = "head: illegal line count -- ";
-    let byteCountError = "head: illegal byte count -- ";
+    let offsetErrorMsg = "tail: illegal offset -- ";
+    let lineCountErrorMsg = "head: illegal line count -- ";
+    let byteCountErrorMsg = "head: illegal byte count -- ";
+
+    let offsetError = offsetErrorMsg + count;
+    let lineCountError = lineCountErrorMsg + count;
+    let byteCountError = byteCountErrorMsg + count;
 
     if (callingContext == "tail") {
-        return count == 0 ? "" : offsetError + count;
+        return count == 0 ? "" : offsetError;
     }
-    return option == "n" ? lineCountError + count : byteCountError + count;
+    return option == "n" ? lineCountError : byteCountError;
 }
 
 const illegalOptionError = function (callingContext, option) {
     let headUsageMsg = "usage: head [-n lines | -c bytes] [file ...]";
     let tailUsageMsg = "usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]";
     let errorMsg = `${callingContext}: illegal option -- ${option}`;
+
     let headErrorMsg = errorMsg + "\n" + headUsageMsg;
     let tailErrorMsg = errorMsg + "\n" + tailUsageMsg;
 
