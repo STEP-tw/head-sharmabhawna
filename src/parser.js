75850a706bb6c4116a178035010d6a7017a3453d
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2018-12-25 10:46:49 +0530

    Modified parse

--- src/parser.js
+++ src/parser.js
@@ -1,43 +1,44 @@
-const onlyFilesSpecified = function (firstArg) {
-  return !firstArg.startsWith("-");
+const isOnlyOption = function (firstArg) {
+  return firstArg.startsWith("-") && isNaN(firstArg[1]);
 };
+
 const isOnlyCount = function (firstArg) {
   return !isNaN(firstArg[1]);
 };
 
 const isOptionWithCount = function (firstArg) {
-  return firstArg.length > 2;
+  return firstArg.startsWith("-") && firstArg.length > 2;
 };
 
 const createFormat = function (option, count, files) {
   return { option, count, files };
 };
+
 const parse = function (userInputs) {
   let firstArg = userInputs[0];
   let secondArg = userInputs[1];
+
   let option = "n";
   let count = 10;
-  let files = userInputs;
+  let files = userInputs.slice(0);
 
-  if (onlyFilesSpecified(firstArg)) {
-    return createFormat(option, count, files);
-  }
-  files = userInputs.slice(1);
-
-  if (isOnlyCount(firstArg)) {
-    let count = firstArg.slice(1);
-    return createFormat(option, count, files);
+  if (isOnlyOption(firstArg)) {
+    option = firstArg[1];
+    count = secondArg;
+    files = userInputs.slice(2);
   }
 
   if (isOptionWithCount(firstArg)) {
-    let option = firstArg.slice(1, 2);
-    let count = firstArg.slice(2);
-    return createFormat(option, count, files);
+    option = firstArg[1];
+    count = firstArg.slice(2);
+    files = userInputs.slice(1);
   }
-  option = firstArg.slice(1);
-  count = secondArg;
-  files = userInputs.slice(2);
 
+  if (isOnlyCount(firstArg)) {
+    option = "n";
+    count = firstArg.slice(1);
+    files = userInputs.slice(1);
+  }
   return createFormat(option, count, files);
 };
 
